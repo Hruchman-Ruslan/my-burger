2ded11e9edb01d356fa8f1543407431c33a0cacc
Ruslan <Giovingo92@gmail.com>
2024-05-09 18:29:00 +0300

    init: init project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to the burger site</h1>
+      <p>🔥 get started! 🔥</p>
+    </main>
+  );
+}
