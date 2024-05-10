e9caf1d002678bc74119938a878c528fa122f80d
Ruslan <Giovingo92@gmail.com>
2024-05-10 21:50:11 +0300

    faet: basic routes

---
+++ src/app/meals/[mealSlug]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface MealDetailsProps {}
+
+export default function MealDetailsPage({}: MealDetailsProps) {
+  return (
+    <main>
+      <h1>Meal Details</h1>
+    </main>
+  );
+}
---
+++ src/app/meals/share/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface ShareMealProps {}
+
+export default function ShareMealPage({}: ShareMealProps) {
+  return (
+    <main>
+      <h1>Share Meal</h1>
+    </main>
+  );
+}
