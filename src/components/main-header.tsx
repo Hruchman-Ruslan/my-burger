abe329d88f4c3c1866b09dc7dd0f16850b3cf0a4
Ruslan <Giovingo92@gmail.com>
2024-05-11 17:51:55 +0300

    feat: optimizing images

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
 
@@ -11,7 +12,7 @@ export default function MainHeader({}: MainHeaderProps) {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel food
       </Link>
 
