2118ee0193cae54ad0c12819f2c48d5d65100e97
Ruslan <Giovingo92@gmail.com>
2024-05-11 17:21:11 +0300

    feat: styles to header

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 
 import logoImg from "@/assets/logo.png";
+
+import classes from "./main-header.module.css";
 
 export interface MainHeaderProps {}
 
 export default function MainHeader({}: MainHeaderProps) {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
         <img src={logoImg.src} alt="A plate with food on it" />
         NextLevel food
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
