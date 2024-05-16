460f3601168847a1d529f3cbd726a6626639a88f
Ruslan <Giovingo92@gmail.com>
2024-05-16 14:42:26 +0300

    feat: handling errors

---
+++ src/app/meals/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export interface ErrorProps {}
+
+export default function Error({}: ErrorProps) {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to fetch meal data. Please try again later.</p>
+    </main>
+  );
+}
