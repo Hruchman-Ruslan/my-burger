5782628d2c9f76491c08b7b50f64ab5494c4c00c
Ruslan <Giovingo92@gmail.com>
2024-05-17 18:05:30 +0300

    feat: image-picker preview

--- src/components/meals/image-picker.tsx
+++ src/components/meals/image-picker.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useRef } from "react";
+import { ChangeEvent, useRef, useState } from "react";
+
+import Image from "next/image";
 
 import classes from "./image-picker.module.css";
 
@@ -10,6 +12,7 @@ export interface ImagePickerProps {
 }
 
 export default function ImagePicker({ label, name }: ImagePickerProps) {
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
   const imageInput = useRef<HTMLInputElement | null>(null);
 
   function handlePickClick() {
@@ -18,10 +21,37 @@ export default function ImagePicker({ label, name }: ImagePickerProps) {
     }
   }
 
+  function handleImageChange(e: ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result as string);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet!</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -29,6 +59,9 @@ export default function ImagePicker({ label, name }: ImagePickerProps) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
+          required
+          //   multiple many files
         />
         <button
           className={classes.button}
