8f991ed814a68d42470755075709dabfc8e8b8d0
Ruslan <Giovingo92@gmail.com>
2024-05-17 13:38:52 +0300

    feat: meal details

--- src/app/meals/[mealSlug]/page.tsx
+++ src/app/meals/[mealSlug]/page.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import Image from "next/image";
 
-export interface MealDetailsProps {}
+import { IMeal } from "@/types/meals";
+import { getMeal } from "@/lib/meals";
 
-export default function MealDetailsPage({}: MealDetailsProps) {
+import classes from "./page.module.css";
+
+export interface MealDetailsProps {
+  params: { mealSlug: string };
+}
+
+export default function MealDetailsPage({ params }: MealDetailsProps) {
+  const meal = getMeal(params.mealSlug) as IMeal;
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1>Meal Details</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            be <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
--- src/types/meals.ts
+++ src/types/meals.ts
@@ -6,3 +6,12 @@ export interface IMeals {
   summary: string;
   creator: string;
 }
+
+export interface IMeal {
+  image: string;
+  title: string;
+  creator_email: string;
+  creator: string;
+  summary: string;
+  instructions: string;
+}
